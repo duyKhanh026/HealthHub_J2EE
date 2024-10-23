3fbe81ee5c30d6494ad68cef446237e4c7f5e667
Lê Hoài Nam <namhoaile2003@gmail.com>
2024-10-23 11:09:19 +0700

    a

--- HealthHub/src/main/java/com/healthhub/hospital/HealthHubApplication.java
+++ HealthHub/src/main/java/com/healthhub/hospital/HealthHubApplication.java
@@ -11,16 +11,7 @@ public class HealthHubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HealthHubApplication.class, args);
 
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-		// Mã hóa mật khẩu
-		String rawPassword = "123";
-		String encodedPassword = passwordEncoder.encode(rawPassword);
-		System.out.println("Mật khẩu đã mã hóa: " + encodedPassword);
-
-		// Kiểm tra mật khẩu
-		boolean isPasswordMatch = passwordEncoder.matches("123", encodedPassword);
-		System.out.println("Mật khẩu khớp: " + isPasswordMatch);
 		
 	}
 
