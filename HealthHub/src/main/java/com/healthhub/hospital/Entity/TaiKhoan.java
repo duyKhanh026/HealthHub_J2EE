012724175e5b16c4edb9dac440952b02696d4f48
duyKhanh026 <117454177+duyKhanh026@users.noreply.github.com>
2024-10-28 23:52:23 +0700

    sửa security

--- HealthHub/src/main/java/com/healthhub/hospital/Entity/TaiKhoan.java
+++ HealthHub/src/main/java/com/healthhub/hospital/Entity/TaiKhoan.java
@@ -3,22 +3,36 @@ package com.healthhub.hospital.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "taikhoan")
 public class TaiKhoan {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MaTK")
     private int maTK;
+
+    @Column(name = "TenDN")
     private String tenDN;
+
+    @Column(name = "Matkhau")
     private String matkhau;
-    private int maBN;  // Đây là khóa ngoại liên kết với bảng BenhNhan
+
+    @OneToOne
+    @JoinColumn(name = "MaBN", insertable = false, updatable = false)
+    private BenhNhan maBN;  // Đây là khóa ngoại liên kết với bảng BenhNhan
+
+    @Column(name = "Vaitro")
     private String vaitro;
 }
--- HealthHub/src/main/java/com/healthhub/hospital/controller/User/RegisterController.java
+++ HealthHub/src/main/java/com/healthhub/hospital/controller/User/RegisterController.java
@@ -1,58 +1,58 @@
-//package com.healthhub.hospital.controller.User;
-//
-//import com.healthhub.hospital.Repository.TaiKhoanRepository;
-//import com.healthhub.hospital.Entity.TaiKhoan;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//@Controller
-//public class RegisterController {
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    private TaiKhoanRepository taiKhoanRepository;
-//
-//    @GetMapping({ "/register" })
-//    public String register(Model model) {
-//        return "User/DangKy";
-//
-//    }
-//
-//    @PostMapping("/register")
-//    public String registerUser(@RequestParam("username") String username,
-//                               @RequestParam("password") String password,
-//                               @RequestParam("confirmPassword") String confirmPassword,
-//                               Model model) {
-//        // Kiểm tra mật khẩu và mật khẩu xác nhận có trùng nhau không
-//        if (!password.equals(confirmPassword)) {
-//            model.addAttribute("error", "Mật khẩu không khớp!");
-//            return "User/DangKy"; // Đưa người dùng trở lại trang đăng ký nếu có lỗi
-//        }
-//
-//        // Kiểm tra xem tên đăng nhập đã tồn tại chưa
-//        TaiKhoan existingAccount = taiKhoanRepository.getAccountByUserName(username);
-//        if (existingAccount != null) {
-//            model.addAttribute("error", "Tên đăng nhập đã tồn tại!");
-//            return "User/DangKy";
-//        }
-//
-//        // Tạo tài khoản mới
-//        TaiKhoan taiKhoan = new TaiKhoan();
-//        taiKhoan.setTenDN(username);
-//        taiKhoan.setMatkhau(passwordEncoder.encode(password)); // Mã hóa mật khẩu
-//        taiKhoan.setVaitro("user"); // Gán vai trò mặc định
-//        taiKhoanRepository.save(taiKhoan);
-//
-//        // Chuyển hướng tới trang đăng nhập sau khi đăng ký thành công
-//        return "redirect:/login";
-//    }
-//
-//}
+package com.healthhub.hospital.controller.User;
+
+import com.healthhub.hospital.Repository.TaiKhoanRepository;
+import com.healthhub.hospital.Entity.TaiKhoan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RegisterController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TaiKhoanRepository taiKhoanRepository;
+
+    @GetMapping({ "/register" })
+    public String register(Model model) {
+        return "User/DangKy";
+
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestParam("username") String username,
+                               @RequestParam("password") String password,
+                               @RequestParam("confirmPassword") String confirmPassword,
+                               Model model) {
+        // Kiểm tra mật khẩu và mật khẩu xác nhận có trùng nhau không
+        if (!password.equals(confirmPassword)) {
+            model.addAttribute("error", "Mật khẩu không khớp!");
+            return "User/DangKy"; // Đưa người dùng trở lại trang đăng ký nếu có lỗi
+        }
+
+        // Kiểm tra xem tên đăng nhập đã tồn tại chưa
+        TaiKhoan existingAccount = taiKhoanRepository.getAccountByTenDN(username);
+        if (existingAccount != null) {
+            model.addAttribute("error", "Tên đăng nhập đã tồn tại!");
+            return "User/DangKy";
+        }
+
+        // Tạo tài khoản mới
+        TaiKhoan taiKhoan = new TaiKhoan();
+        taiKhoan.setTenDN(username);
+        taiKhoan.setMatkhau(passwordEncoder.encode(password)); // Mã hóa mật khẩu
+        taiKhoan.setVaitro("user"); // Gán vai trò mặc định
+        taiKhoanRepository.save(taiKhoan);
+
+        // Chuyển hướng tới trang đăng nhập sau khi đăng ký thành công
+        return "redirect:/login";
+    }
+
+}
