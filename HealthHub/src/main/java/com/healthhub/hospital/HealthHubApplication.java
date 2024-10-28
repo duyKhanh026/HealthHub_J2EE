e3a2f67acf4e769de147fd0af03c2c6be1a1a8c7
duyKhanh026 <117454177+duyKhanh026@users.noreply.github.com>
2024-10-28 23:34:56 +0700

    tách controller

--- HealthHub/src/main/java/com/healthhub/hospital/HealthHubApplication.java
+++ HealthHub/src/main/java/com/healthhub/hospital/HealthHubApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HealthHubApplication {
 	public static void main(String[] args) {
--- HealthHub/src/main/java/com/healthhub/hospital/controller/MainController.java
+++ HealthHub/src/main/java/com/healthhub/hospital/controller/MainController.java
@@ -12,21 +12,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 //import com.healthhub.hospital.Entity.BenhNhan;
 import com.healthhub.hospital.Entity.ChiTietLichKham;
+import com.healthhub.hospital.Entity.LichKham;
 //import com.healthhub.hospital.Entity.LichKham;
 //import com.healthhub.hospital.service.BenhNhanService;
 import com.healthhub.hospital.service.ChiTietLichKhamService;
+import com.healthhub.hospital.service.LichKhamService;
 //import com.healthhub.hospital.service.LichKhamService;
 
 @Controller
 public class MainController {
 //	@Autowired
 //	private BenhNhanService benhNhanserv;
-//
-//	@Autowired
-//	private LichKhamService lichKhamService;
-
-//	@Autowired
-//	private ChiTietLichKhamService chiTietLichKhamService;
 
 	// Inject via application.properties
 	@Value("${welcome.message}")
@@ -46,26 +42,6 @@ public class MainController {
 		return "User/index";
 	}
 
-//	@GetMapping({ "/LichKhamLS" })
-//	public String lichKham(Model model) {
-//
-//		List<LichKham> listkham = new ArrayList<>();
-//		listkham = lichKhamService.getAllLichKham();
-//		model.addAttribute("listkham", listkham);
-//	    model.addAttribute("lichkhamchitiet", new ChiTietLichKham());
-//		return "User/LichKham";
-//	}
-//	@GetMapping("/LichKhamLS/chiTiet")
-//	public String chiTietLichKham(@RequestParam Integer id, Model model) {
-//		ChiTietLichKham lk = chiTietLichKhamService.getChiTietLichKhamByMaLK(id).get(0);
-//
-//	    if (lk == null) {
-//	        return "error/404";
-//	    }
-//
-//	    model.addAttribute("lichkhamchitiet", lk);
-//	    return "User/LichKham :: #lichkhamContent";  // Chỉ trả về fragment chứa chi tiết
-//	}
 //
 //	@GetMapping({ "/make_appointment" })
 //	public String make_appoint(Model model) {
