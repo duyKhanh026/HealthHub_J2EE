c67d2b21d5d0ea26a8888d7bb2aa7b466313b21a
duyKhanh026 <117454177+duyKhanh026@users.noreply.github.com>
2024-10-28 23:59:36 +0700

    BULL SHITT

--- HealthHub/src/main/java/com/healthhub/hospital/controller/MainController.java
+++ HealthHub/src/main/java/com/healthhub/hospital/controller/MainController.java
@@ -1,30 +1,13 @@
 package com.healthhub.hospital.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-//import com.healthhub.hospital.Entity.BenhNhan;
-import com.healthhub.hospital.Entity.ChiTietLichKham;
-import com.healthhub.hospital.Entity.LichKham;
-//import com.healthhub.hospital.Entity.LichKham;
-//import com.healthhub.hospital.service.BenhNhanService;
-import com.healthhub.hospital.service.ChiTietLichKhamService;
-import com.healthhub.hospital.service.LichKhamService;
-//import com.healthhub.hospital.service.LichKhamService;
 
 @Controller
 public class MainController {
-//	@Autowired
-//	private BenhNhanService benhNhanserv;
 
-	// Inject via application.properties
 	@Value("${welcome.message}")
 	private String message;
 
@@ -35,19 +18,7 @@ public class MainController {
 	@GetMapping({ "/", "/index" })
 	public String index(Model model) {
 
-//		List<BenhNhan> benhnhans = benhNhanserv.getAllBenhNhan();
-//		model.addAttribute("benhnhans", benhnhans);
-
-
 		return "User/index";
 	}
-
-//
-//	@GetMapping({ "/make_appointment" })
-//	public String make_appoint(Model model) {
-//
-//		return "User/make_appointment";
-//	}
-
 
 }
